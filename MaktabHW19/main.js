a9d900f5182191cc9814c3dd44ed4522b8d47963
Farshad-FC <fastcode.co@gmail.com>
2022-09-13 00:37:11 +0430

    add json file

--- MaktabHW19/main.js
+++ MaktabHW19/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    $.getJSON('ir.json', function (data) {
+        var $select = $('#cities');
+        $.each(data.city, function (a, b) {
+            var $option = $("<option/>").attr("value", b.city).text(b.city);
+            $select.append($option);
+        });
+    });
+    // console.log(2);
     document.getElementById("btn").onclick = function () {
         geo()
     };
@@ -11,16 +19,10 @@ $(document).ready(function () {
             url: "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=c0e42332729c344a89b55f4fd7fe1c86",
             async: false,
             success: function (response) {
-                console.log(response[0].name);
-                console.log(response[0].lat);
-                console.log(response[0].lon);
-
                 $.ajax({
                     type: "GET",
                     url: "https://api.openweathermap.org/data/2.5/weather?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=c0e42332729c344a89b55f4fd7fe1c86",
                     success: function (response) {
-                        console.log(response);
-
                         var tdHtml;
                         tdHtml = tdHtml + `<tr>
                         <td>${response.cod}</td>
