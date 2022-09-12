b5eb48cb0e7d3252902efc5224acd5c811d66585
Farshad-FC <fastcode.co@gmail.com>
2022-09-12 22:01:07 +0430

    add weather all files

---
+++ MaktabHW19/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    document.getElementById("btn").onclick = function () {
+        geo()
+    };
+    var city;
+
+    function geo() {
+        var cityName = document.getElementById("cities").value;
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=c0e42332729c344a89b55f4fd7fe1c86",
+            async: false,
+            success: function (response) {
+                console.log(response[0].name);
+                console.log(response[0].lat);
+                console.log(response[0].lon);
+
+                $.ajax({
+                    type: "GET",
+                    url: "https://api.openweathermap.org/data/2.5/weather?lat=" + response[0].lat + "&lon=" + response[0].lon + "&appid=c0e42332729c344a89b55f4fd7fe1c86",
+                    success: function (response) {
+                        console.log(response);
+
+                        var tdHtml;
+                        tdHtml = tdHtml + `<tr>
+                        <td>${response.cod}</td>
+                        <td>${response.name}</td>
+                        <td>${response.weather[0].id}</td>
+                        <td>${response.weather[0].main}</td>
+                        <td>${response.weather[0].description}</td>
+                        <td>${response.wind.speed}</td>
+                        <td>${response.wind.deg}</td>
+                        </tr>`
+
+                        $("#tableBody").html(
+                            tdHtml
+                        );
+                    }
+                });
+            }
+        });
+    }
+});
